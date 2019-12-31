914f75a72b65f06a77896364503265a4fa57f422
vennam prasad <vennamprasad@gmail.com>
2019-12-31 18:58:25 +0530

    Initial commit

---
+++ app/src/main/java/com/apphigh/feedly/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package com.apphigh.feedly.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
